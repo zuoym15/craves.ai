7b63aba768f62dda4f5301b1c175a24db5d28183
zuoym15 <zuoyiming16@sina.com>
2019-04-04 12:36:32 +0800

    update train and test script

--- train_arm.sh
+++ train_arm.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python train_arm.py -a hg --stacks 2 --blocks 1 --num-classes 17 --checkpoint ./checkpoint/arm/20190206 --data-dir ./data/20181107 --meta-dir ./data/meta/17_vertex --epoch 30 --schedule 20 --train-batch 6 --test-batch 6 --anno-type 3d --training-set-percentage 0.5
+CUDA_VISIBLE_DEVICES=0 python train_arm.py -a hg --stacks 2 --blocks 1 --num-classes 17 --checkpoint ./checkpoint/arm/20190206 --data-dir ./data/20181107 --meta-dir ./data/meta/17_vertex --epoch 30 --schedule 20 --train-batch 6 --test-batch 6 --anno-type 3d --training-set-percentage 0.9

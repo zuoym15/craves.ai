131af48b5e68c1b5f4478b3679f0287da2da7eff
zuoym15 <zuoyiming16@sina.com>
2019-06-15 22:17:06 +0800

    solve the 2d keypoint bias bug

--- pose/utils/transforms.py
+++ pose/utils/transforms.py
@@ -122,8 +122,8 @@ def transform(pt, center, scale, res, invert=0, rot=0):
     t = get_transform(center, scale, res, rot=rot)
     if invert:
         t = np.linalg.inv(t)
-    # new_pt = np.array([pt[0] - 1, pt[1] - 1, 1.]).T
-    new_pt = np.array([pt[0], pt[1], 1.]).T
+    new_pt = np.array([pt[0] - 1, pt[1] - 1, 1.]).T
+    #new_pt = np.array([pt[0], pt[1], 1.]).T
     new_pt = np.dot(t, new_pt)
     # return new_pt[:2].astype(int) + 1
     return (new_pt[:2] + 0.5).astype(int)
